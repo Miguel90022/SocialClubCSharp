40c2bb78e5e2ad68c4237696f5d675b7aa5a3d66
Miguel Angel Diaz <100099435+Miguel90022@users.noreply.github.com>
2024-02-12 20:24:32 -0500

    Add files via upload

---
+++ clubSocial.sql
@@ -0,0 +1,125 @@
+create database clubSocial
+
+go
+
+use clubSocial
+
+go
+
+create table socio(
+
+pkCedulaSocio int primary key,
+
+tipoSuscripcionSocio varchar(10) not null,
+
+fondosDisponiblesSocio float not null,
+
+nombreSocio varchar(100) not null,
+
+)
+
+go
+
+create table consumoSocio(
+
+pkIdConsumo int identity primary key,
+
+nombreConsumo varchar(100) not null,
+
+precioConsumo float not null,
+
+nombreResponsableConsumo varchar(100) not null,
+
+fkCedulaSocioConsumo int foreign key references socio(pkCedulaSocio)not null
+
+)
+
+go
+
+create table personaAutorizada(
+
+pkCedulaPersonaAutorizada int primary key,
+
+fkCedulaSocioResponsable int foreign key references socio(pkCedulaSocio) not null,
+
+nombrePersonaAutorizada varchar(100) not null,
+
+)
+
+go
+
+create table consumoPersonaAutorizada(
+
+pkIdConsumo int identity primary key,
+
+nombreConsumo varchar(100) not null,
+
+precioConsumo float not null,
+
+nombreResponsableConsumo varchar(100) not null,
+
+fkCedulaPersonaAutorizadaConsumo int foreign key references personaAutorizada(pkCedulaPersonaAutorizada) not null
+
+)create database clubSocial
+
+go
+
+use clubSocial
+
+go
+
+create table socio(
+
+    pkCedulaSocio int primary key,
+
+    tipoSuscripcionSocio varchar(10) not null,
+
+    fondosDisponiblesSocio float not null,
+
+    nombreSocio varchar(100) not null,
+
+)
+
+go
+
+create table consumoSocio(
+
+    pkIdConsumo int identity primary key,
+
+    nombreConsumo varchar(100) not null,
+
+    precioConsumo float not null,
+
+    nombreResponsableConsumo varchar(100) not null,
+
+    fkCedulaSocioConsumo int foreign key references socio(pkCedulaSocio)not null
+
+)
+
+go
+
+create table personaAutorizada(
+
+    pkCedulaPersonaAutorizada int primary key,
+
+    fkCedulaSocioResponsable int foreign key references socio(pkCedulaSocio) not null,
+
+    nombrePersonaAutorizada varchar(100) not null,
+
+)
+
+go
+
+create table consumoPersonaAutorizada(
+
+    pkIdConsumo int identity primary key,
+
+    nombreConsumo varchar(100) not null,
+
+    precioConsumo float not null,
+
+    nombreResponsableConsumo varchar(100) not null,
+
+    fkCedulaPersonaAutorizadaConsumo int foreign key references personaAutorizada(pkCedulaPersonaAutorizada) not null
+
+)
